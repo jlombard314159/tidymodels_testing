dcaf6e9914592832189cf350a16c2d87db561c57
jlombard314159 <jlombard314@gmail.com>
2023-05-14 12:07:50 -0600

    some more work to set data up

--- tidymodels/data_loading.R
+++ tidymodels/data_loading.R
@@ -3,15 +3,22 @@ rm(list=ls())
 library(tidymodels)
 library(dplyr)
 library(ggplot2)
-
+options(tibble.width = Inf)
 
 batting <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/Batting.csv')
 parent_data <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/Master.csv')
-pitching <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/Pitching.csv')
+# pitching <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/Pitching.csv')
 
 batting$SF[is.na(batting$SF)] <- 0
 batting$IBB[is.na(batting$IBB)] <- 0
 
+#Helpful to have things sorted by year f or each playerID
+batting <- batting %>% group_by(playerID) %>% 
+  arrange(yearID)
+
+#Injuries -> steroid use. Use low year to normal year as a surrogate
+batting <- batting %>% group_by(playerID) %>% 
+  mutate(PreviousYearInjury = AB - lag(AB))
 
 batting <- batting %>% filter(yearID > 1945) %>% 
   filter(AB > 130) %>% 
@@ -27,21 +34,33 @@ short_stint <- batting %>% group_by(playerID) %>%
 
 batting <- batting %>%  filter(playerID %in% short_stint$playerID)
 
-#As expected
-batting[batting$OPS > 1.3,]
+#Steroid specific information
+#Add in known steroid users
+known_steroids <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/mlb_steroid_users.csv')
+
+parent_data$FullName <- paste0(parent_data$nameFirst,' ',parent_data$nameLast)
+
+batting$FullName <- parent_data$FullName[match(paste0(batting$playerID),
+                                                       paste0(parent_data$playerID))]
+
+batting$KnownSteroid <- 0
+batting$KnownSteroid[batting$FullName %in% known_steroids$Player.Name] <- 1
 
 batting$SteroidEra <- 0
 batting$SteroidEra[batting$yearID > 1993 & batting$yearID < 2006] <- 1
 batting$SteroidEra <- as.factor(batting$SteroidEra)
 
-plot_metric_by_year <- function(data,metric){
-  
-  
-}
-
-ggplot(batting, aes(x = yearID, y =OPS, colour = SteroidEra)) + 
-  geom_point(alpha = 0.25)
-  geom_smooth()
+#As expected
+batting[batting$OPS > 1.3,]
+# 
+# plot_metric_by_year <- function(data,metric){
+#   
+#   
+# }
+# 
+# ggplot(batting, aes(x = yearID, y =OPS, colour = SteroidEra)) + 
+#   geom_point(alpha = 0.25)
+#   geom_smooth()
 
 
 #-------------------------------------------------------------------------------
@@ -49,33 +68,24 @@ ggplot(batting, aes(x = yearID, y =OPS, colour = SteroidEra)) +
   
 #-------------------------------------------------------------------------------#
 #Where to start. Maybe the average rate of change of OPS is too high
-
 batting <- batting %>% group_by(playerID) %>% 
   mutate(OPS_Change = OPS - lag(OPS))
 
 #Lets define an outlier: OPS is high (?) and the avg ROC of OPS is abnormally high
 #abnormal: top 90% quantile
 
-batting_summary <- batting %>% group_by(playerID) %>% 
+batting_summary <- batting %>% group_by(FullName) %>% 
   summarize(OPS = mean(OPS), HR = mean(HR), OPS_Change_Avg = mean(OPS_Change, na.rm=TRUE),
             SLUG = mean(SLUG), OPS_Incr_Perc = sum(OPS_Change > 0, na.rm=TRUE)/(length(yearID) - 1),
-            Years = length(yearID))
+            Years = length(yearID),
+            PrevInjuryCount = sum(PreviousYearInjury > 150, na.rm=TRUE)/(length(yearID)-1),
+            KnownSteroid = unique(KnownSteroid))
 
 batting_summary[batting_summary$OPS_Incr_Perc > 0.8 & 
                   batting_summary$Years > 5,]
 
 batting_summary[batting_summary$HR > 35,]
 
-#Add in known steroid users
-known_steroids <- read.csv('C:/Users/jlomb/Documents/Personal Coding/tidymodels/mlb_steroid_users.csv')
-
-parent_data$FullName <- paste0(parent_data$nameFirst,' ',parent_data$nameLast)
-
-batting_summary$FullName <- parent_data$FullName[match(paste0(batting_summary$playerID),
-                                                       paste0(parent_data$playerID))]
-
-batting_summary$KnownSteroid <- 0
-batting_summary$KnownSteroid[batting_summary$FullName %in% known_steroids$Player.Name] <- 1
 
 #-------------------------------------------------------------------------------
 #Some modeling
@@ -92,6 +102,13 @@ data_split <- initial_split(batting_summary, prop = 3/4)
 train_data <- training(data_split)
 test_data  <- testing(data_split)
 
+steroid_use <- recipe(KnownSteroid ~ .,
+                      data = train_data) 
+
+steroid_use <- steroid_use %>% 
+  update_role(FullName, new_role = 'Player Name')
+
+summary(steroid_use)
 
 
 #---------------
